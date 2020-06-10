5c1eb65b65ae0b2ea76f13b1972bcb0845b268bd
SeninAlexey <58198328+Zzeyrol@users.noreply.github.com>
2020-06-10 14:59:13 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,18 +1,15 @@
    --Запрос №2
-SELECT * FROM bookshelf.books;
 SELECT  author, title 
 From books
 GROUP BY author 
 ORDER BY author;
 
    --Запрос №3
-SELECT * FROM bookshelf.books;
 SELECT author, COUNT(title)
 FROM books
 WHERE author = 'Вьяса';
 
    --Запрос №4
-SELECT * FROM bookshelf.books;
 SELECT author, 
 COUNT(author) AS title
 FROM books
@@ -21,7 +18,6 @@ HAVING title > 1
 ORDER BY title DESC ;
 
    --Запрос №5
-SELECT * FROM bookshelf.books;
 SELECT id, author, title
 FROM books
 WHERE author = 'Лев Толстой'
@@ -32,13 +28,11 @@ WHERE id = '96'
 /*Удалил копию Войны и мира*/
 
    --Запрос №6
-SELECT * FROM bookshelf.books;
 SELECT author, shelflocation
 FROM books
 WHERE shelflocation = 'Гостиная'
 
    --Запрос №7
-SELECT * FROM bookshelf.books;
 SELECT year, title 
 FROM books
 WHERE year > 0
@@ -46,12 +40,10 @@ ORDER BY year ASC
 LIMIT 5
 
    --Запрос №8
-SELECT * FROM bookshelf.books;
 SELECT COUNT(title), shelflocation
 FROM books
 WHERE shelflocation = 'Гостиная'
 /*Вывел обшее кол-во книг на полке*/
-SELECT * FROM bookshelf.books;
 SELECT COUNT(title), shelflocation
 FROM books
 /*Вывел обшее кол-во книг на всех полках*/
