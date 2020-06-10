8982775352418a145e42d0abfc9ddaaa024922b1
SeninAlexey <58198328+Zzeyrol@users.noreply.github.com>
2020-06-10 14:14:12 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,17 +1,17 @@
-    Запрос №2
+   --Запрос №2
 SELECT * FROM bookshelf.books;
 SELECT  author, title 
 From books
 GROUP BY author 
 ORDER BY author;
 
-    Запрос №3
+   --Запрос №3
 SELECT * FROM bookshelf.books;
 SELECT author, COUNT(title)
 FROM books
 WHERE author = 'Вьяса';
 
-    Запрос №4
+   --Запрос №4
 SELECT * FROM bookshelf.books;
 SELECT author, 
 COUNT(author) AS title
@@ -20,7 +20,7 @@ GROUP BY author
 HAVING title > 1
 ORDER BY title DESC ;
 
-    Запрос №5
+   --Запрос №5
 SELECT * FROM bookshelf.books;
 SELECT id, author, title
 FROM books
@@ -31,13 +31,13 @@ DELETE id from books
 WHERE id = '96' 
 /*Удалил копию Войны и мира*/
 
-    Запрос №6
+   --Запрос №6
 SELECT * FROM bookshelf.books;
 SELECT author, shelflocation
 FROM books
 WHERE shelflocation = 'Гостиная'
 
-    Запрос №7
+   --Запрос №7
 SELECT * FROM bookshelf.books;
 SELECT year, title 
 FROM books
@@ -45,7 +45,7 @@ WHERE year > 0
 ORDER BY year ASC
 LIMIT 5
 
-    Запрос №8
+   --Запрос №8
 SELECT * FROM bookshelf.books;
 SELECT COUNT(title), shelflocation
 FROM books
